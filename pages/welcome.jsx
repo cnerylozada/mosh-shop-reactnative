917838e38bad52fc8273144276d596f481c6c04b
cnerylozada <cnery@dynamicdevs.io>
2021-02-14 10:16:09 -0500

    feat: image-bg and buttons were implemented

--- pages/welcome.jsx
+++ pages/welcome.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 import { palette } from "../styles";
 
 const WelcomePage = () => {
   return (
     <View style={styles.container}>
-      <Text>Welcome page</Text>
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("../styles/images/background.jpg")}
+      >
+        <View style={styles.buttonContainer}>
+          <View style={styles.login}>
+            <Text>Login</Text>
+          </View>
+          <View style={styles.register}>
+            <Text>Register</Text>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -14,6 +26,19 @@ export default WelcomePage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    position: "relative",
+  },
+  login: {
+    backgroundColor: palette.red,
+    padding: 20,
+  },
+  register: {
     backgroundColor: palette.green,
+    padding: 20,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
   },
 });
